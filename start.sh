0123427957081309c132da6b6909085ef1e01b57
zhanglinw0822 <zhanglinw0822@hotmail.com>
2016-06-27 16:40:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d -p port:50001 -v /opt/logs/appname:/opt/app/appname/logs --name appname push_docker_registry/docker_repository_name:docker_tag
+docker run -d -p 50001:50001 -v /opt/logs/JReuters:/opt/app/JReuters/logs --name JReuters 192.168.1.112:5000/test/hqzf_config:docker_tag
