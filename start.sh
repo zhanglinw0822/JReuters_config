5bb9b28958143dae41ead07a13ea58c68a190946
zhanglinw0822 <zhanglinw0822@hotmail.com>
2016-06-27 16:30:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d -p 50001:50001 -v /opt/logs/JReuters:/opt/app/JReuters/logs --name JReuters push_docker_registry/docker_repository_name:docker_tag
+docker run -d -p port:50001 -v /opt/logs/appname:/opt/app/appname/logs --name appname push_docker_registry/docker_repository_name:docker_tag
